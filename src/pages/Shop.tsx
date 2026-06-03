e3285f835c4193f5cb0222fd1d27c9aab0b2874b
SuperGame22 <supersourcesocialmedia@gmail.com>
2026-06-02 19:09:34 -0700

    Restoring Pages part 2

---
+++ src/pages/Shop.tsx
@@ -0,0 +1,3 @@
+import { AppLayout } from "@/components/layout/AppLayout";
+import { Button } from "@/components/ui/button";
+export default function Shop() { return <AppLayout><div className="p-8"><h1 className="text-2xl font-bold">Shop</h1></div></AppLayout>; }
